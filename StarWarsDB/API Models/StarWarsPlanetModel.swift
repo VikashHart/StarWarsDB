da070eb1f66c4f6f41912dbce1c49b383788aee6
Vikash Hart <vikashhart@gmail.com>
2019-08-10 11:48:56 -0400

    api models added

---
+++ StarWarsDB/API Models/StarWarsPlanetModel.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+struct StarWarsPlanetModel: Codable {
+    let results: [PlanetData]
+}
+
+struct PlanetData: Codable {
+    let name: String
+    let rotation_period: String
+    let orbital_period: String
+    let diameter: String
+    let climate: String
+    let gravity: String
+    let terrain: String
+    let surface_water: String
+    let population: String
+    let created: String
+
+    enum CodingKey: String, Codable {
+        case name
+        case rotation_period = "rotationPeriod"
+        case orbital_period = "orbitalPeriod"
+        case diameter
+        case climate
+        case gravity
+        case terrain
+        case surface_water = "surfaceWater"
+        case population
+        case created = "dataCreatedDate"
+    }
+}
