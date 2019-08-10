42a7b15f0dff3ca34991ea62ab7899d18f460e7d
Vikash Hart <vikashhart@gmail.com>
2019-08-10 10:56:18 -0400

    Initial Commit

---
+++ StarWarsDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsDB
+//
+//  Created by C4Q on 8/10/19.
+//  Copyright © 2019 Vikash Hart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
